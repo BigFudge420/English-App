07ed11cca52528d61e91ad5e37d63c9054e77115
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-08-29 00:04:40 +0530

    Set up basic login page

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -1,9 +1,28 @@
+import {auth} from "../config/firebase"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
+import {getFirestore, doc, getDoc} from "firebase/firestore"
+import { useState } from "react"
+
+const db = getFirestore()
+
 export const Auth = ()=>{
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    async function logIn(email ,password){
+        
+        const userCred = await signInWithEmailAndPassword(auth, email, password)
+        console.log("Logged in as:", userCred.user.uid)
+
+    } 
+    
     return(
         <div>
-            <input placeholder="Username..." />
-            <input placeholder="Password..." />
-            <button>Log in</button>
+            <input placeholder="Username..." onChange={(e)=>setEmail(e.target.value)} />
+            <input placeholder="Password..." onChange={(e)=>setPassword(e.target.value)}/>
+            <button onClick={() => logIn(email, password)}>Log in</button>
         </div>
-    )
+    )    
 }
+
+    
