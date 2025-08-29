80283befe5540582d5485342469f7e38bf1a3893
BigFudge420 <87513995+BigFudge420@users.noreply.github.com>
2025-08-29 09:22:25 +0530

    Use username for login instead of email

--- src/components/auth.jsx
+++ src/components/auth.jsx
@@ -3,26 +3,34 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import {getFirestore, doc, getDoc} from "firebase/firestore"
 import { useState } from "react"
 
-const db = getFirestore()
-
 export const Auth = ()=>{
-    const [email, setEmail] = useState("")
+    const [user, setUser] = useState("")
     const [password, setPassword] = useState("")
 
-    async function logIn(email ,password){
-        
-        const userCred = await signInWithEmailAndPassword(auth, email, password)
-        console.log("Logged in as:", userCred.user.uid)
+    async function logIn(user,password){
+        const email = `${user}@myapp.local` 
 
-    } 
+        try{
+            const userCred = await signInWithEmailAndPassword(auth, email, password)
+            console.log("Logged in as:", userCred.user.uid)
+        }catch (err){
+            if (err.code === "auth/invalid-email"){
+                console.error("❌ User does not exist")
+            }
+            else if (err.code === "auth/invalid-credential") {
+                console.error("❌ Wrong password for that user")
+            } else {
+                console.error("Other error:", err.message)
+            }
+        } 
+    }   
     
     return(
         <div>
-            <input placeholder="Username..." onChange={(e)=>setEmail(e.target.value)} />
+            <input placeholder="Username..." onChange={(e)=>setUser(e.target.value)} />
             <input placeholder="Password..." onChange={(e)=>setPassword(e.target.value)}/>
-            <button onClick={() => logIn(email, password)}>Log in</button>
+            <button onClick={() => logIn(user, password)}>Log in</button>
         </div>
     )    
 }
 
-    
